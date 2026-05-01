7c196c18090dea97f2785146efe96af162088bf8
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-01 18:31:20 +0200

    -

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_CppStudio_FILE /var/home/tiwut/Documents/Dev/CPP-Studio/build/CppStudio)
-set(__QT_DEPLOY_TARGET_CppStudio_TYPE EXECUTABLE)
--- build/CMakeFiles/CppStudio.dir/CppStudio_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CppStudio.dir/CppStudio_autogen/mocs_compilation.cpp.o: \
- /var/home/tiwut/Documents/Dev/CPP-Studio/build/CppStudio_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
